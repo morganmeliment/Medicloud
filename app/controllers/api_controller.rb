83b563465fcc98dddfb3357b11ef4aee9d356aea
morgan <morganmeliment@gmail.com>
2016-03-17 21:09:13 -0400

    dsagh

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -385,7 +385,7 @@ p {
     color: #3ab64d;
 }
 	</style>
-	<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'></script>
+	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js\"></script>
 	<script>
 	function refreshtimeline() {
 		$.ajax({
